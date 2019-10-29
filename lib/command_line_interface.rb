6ee75f703bfbb42704c8cf89c8e15a5204db8b95
singingcactus <bechma02@gmail.com>
2019-10-29 16:29:46 -0400

    let's hope this works

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "May the Force Be With You"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
